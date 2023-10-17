dc026467150fc00b6d69ce096775e5561f7a441b
Grace Lee <glee3890@gmail.com>
2023-10-17 08:31:11 -0400

    Add description and Welcome Print Lines

--- ProjectOrangutan.py
+++ ProjectOrangutan.py
@@ -1,1 +1,8 @@
- 
+"""
+Our Welcome Screen will start our program letting
+drivers know that the InfoTech Center 2023 is loading
+"""
+
+print("\nWelcome - InfoTech Center 2023")
+
+print("\nInfoTech Center 2023 is loading...")
