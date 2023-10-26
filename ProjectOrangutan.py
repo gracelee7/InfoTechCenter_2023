dbc3528e94a3adf9b240bdb118e2c96b1934842a
Grace Lee <glee3890@gmail.com>
2023-10-26 08:25:06 -0400

    Add While Loop to load OS

--- ProjectOrangutan.py
+++ ProjectOrangutan.py
@@ -5,9 +5,24 @@ drivers know that the InfoTech Center 2023 is loading
 
 #Import Libraries Here
 import time
+import sys
 
 timeToSleep = 2
 
-print("\nWelcome - InfoTech Center 2023")
+print("\nWelcome - InfoTech Center 2023\n")
 time.sleep(timeToSleep)
-print("\nInfoTech Center 2023 is loading...")
+
+#Add Code to have the ellipses add a dot every .5 seconds
+x = 0
+ellipsis = 0
+
+while x != 20:
+    x += 1
+    message = ("InfoTech Center 2023 is loading" + "." * ellipsis)
+    ellipsis = ellipsis + 1
+    sys.stdout.write("\r" + message) #\r prints a  carriage return first. so, message is printed on top of the previous line
+    time.sleep(.5)
+    if ellipsis == 4:
+        ellipsis = 0
+    if x == 20:
+        print("\n\nOperating System Loaded - Retina Scanned - Access Granted")
