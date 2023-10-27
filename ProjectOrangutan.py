7eed7cc540ec256daa840280c3b1af27894bdbc2
Grace Lee <glee3890@gmail.com>
2023-10-27 08:38:51 -0400

    Imported Libraries and Defined a gasLevelGuage Function

--- ProjectOrangutan.py
+++ ProjectOrangutan.py
@@ -1,1 +1,12 @@
- 
+print("*********************************************")
+print("Gasoline Branch\n\n")
+
+#Import Libraries Here
+import random
+from time import sleep
+
+def gasLevelGuage():
+    gasLevelList = ["Empty","Low","Quarter Tank","Half Tank","Three Quarter Tank","Full Tank"]
+    currentGasLevel = random.choice(gasLevelList)
+    return currentGasLevel
+
